600b077dbc90641aecd010813d822f28b821e272
JoseFernandes975 <zehenrique752005@gmail.com>
2022-09-13 12:24:19 -0300

    Referenciando os posts do usuarios

--- src/main/java/com/educandoweb/course/config/Instantiation.java
+++ src/main/java/com/educandoweb/course/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.educandoweb.course.domain.Post;
 import com.educandoweb.course.domain.User;
+import com.educandoweb.course.dto.AuthorDTO;
 import com.educandoweb.course.repository.PostRepository;
 import com.educandoweb.course.repository.UserRepository;
 
@@ -33,12 +34,16 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("13/09/2022"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("14/09/2022"), "Bom Dia!","Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria ,alex ,bob));
 		
+		Post post1 = new Post(null, sdf.parse("13/09/2022"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("14/09/2022"), "Bom Dia!","Acordei feliz hoje!", new AuthorDTO(maria));
 		
-     	userRepository.saveAll(Arrays.asList(maria ,alex ,bob));
      	postRepository.saveAll(Arrays.asList(post1, post2));
+     	
+     	maria.getPosts().addAll((Arrays.asList(post1, post2)));
+     	userRepository.save(maria);
+     	
    }
 	
 
